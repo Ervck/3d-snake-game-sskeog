025345d4b5d00e733afee951dd66ed5b93593043
Ervck <erickmorales200120@gmail.com>
2021-02-16 18:44:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3d-snake-game-sskeog
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3d-snake-game-sskeog)
---
+++ Snake.ts
@@ -0,0 +1,69 @@
+import display from './display';
+
+// place your code on line 5 above the export statement below
+
+class Point {
+  private xcoord: number;
+  private ycoord: number;
+
+
+ constructor(x = 0, y = 0) {
+    this.xcoord = x;
+    this.ycoord = y;
+
+ }
+get xc() {
+    return this.xcoord;
+ }
+get yc() {
+    return this.ycoord;
+
+ }
+
+
+
+
+
+}
+
+
+
+
+
+class Snake {
+
+private currentPosition: number;
+private currentDirection: number;
+private initialLocation: ;
+  constructor() {
+    this.currentPosition = 0;
+    this.currentDirection = 1;
+    this.initialLocation = new Point ()
+
+  }
+
+public move(pos:number) {
+   display("The Snake Slithers Sinisterly");
+   if (this.currentDirection == 1) {this.currentPosition == this.currentPosition + pos, display("The snake has moved foward by: ", pos, "space(s). To A New Position Of: ", this.currentPosition);}
+
+   else {this.currentPosition == this.currentPosition - pos, display("The snake has moved backwards by: ", pos, "space(s). To A New Position Of: ", this.currentPosition);}
+
+  }
+public turn() {
+   if (this.currentDirection == 1) {this.currentDirection == this.currentDirection - 2, display("The Snake Whips It's Tail, Turning Backward. ");}
+
+   else {this.currentDirection == this.currentDirection + 2, display("The Snake Whips It's Tail, Turning Foward. ");}
+
+}
+
+
+public get position() {
+ return this.currentPosition
+}
+
+set pos2(newPosition:number) {
+  this.currentPosition = newPosition; }
+
+}
+
+export default Snake;
